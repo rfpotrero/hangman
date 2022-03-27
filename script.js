d2f6f5aff41bf3fa80f6425920bc72aef422fa7f
Raul <raul@Rauls-MacBook-Pro.local>
2022-03-27 14:12:36 +0100

    Add event listener to the selection buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function gameSelection(){
-
+function gameSelection(event){
+  
 }
 
 function letterSelection(){
@@ -13,3 +13,20 @@ function hintDisplay(){
 function playAgain(){
     
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+
+        button.addEventListener("click", function() {
+           let gameType = this.innerHTML;
+            alert(`You clicked ${gameType}`);
+            // Pending to add a redirect to the game.html.
+        });
+       
+    }
+});
+
