87c6d62e88137201760639d1333b8dbec5919fdc
Raul <raul@Rauls-MacBook-Pro.local>
2022-03-28 11:26:23 +0100

    Add comments to hmtl and css files

--- script.js
+++ script.js
@@ -14,14 +14,17 @@ function playAgain(){
     
 }
 
+let sourceMovies = ["Terminator", "The Dark Knight"]
+let sourceComics = ["Batman", "Superman"]
+
 document.addEventListener("DOMContentLoaded", function() {
     
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
-           let gameType = this.innerHTML;
-            alert(`You clicked ${gameType}`);
+           let sourceType = this.innerHTML;
+            alert(`You clicked ${sourceType}`);
             // Pending to add a redirect to the game.html.
         });
     }
