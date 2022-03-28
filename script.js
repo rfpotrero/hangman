df96f9bdbc88041d18a242ff0e1260fcb1f9f207
Raul <raul@Rauls-MacBook-Pro.local>
2022-03-28 11:19:09 +0100

    Add style to game.html

--- script.js
+++ script.js
@@ -14,19 +14,16 @@ function playAgain(){
     
 }
 
-
 document.addEventListener("DOMContentLoaded", function() {
     
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-
         button.addEventListener("click", function() {
            let gameType = this.innerHTML;
             alert(`You clicked ${gameType}`);
             // Pending to add a redirect to the game.html.
         });
-       
     }
 });
 
