685763a5356da5661cbce1d9ea76ef1533e00e46
Raul <raul@Rauls-MacBook-Pro.local>
2022-03-28 17:12:03 +0100

    Add code to select a random item in the movie array after clicking in the movie button

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function gameSelection(event){
-  
+// This function select random item in the array to use as a challenge.
+function newGame(sourceType){
+
+    if (sourceType === "Movies"){
+    let challengeIndex = getRandomInt();
+    let challengeArray = sourceMovies[challengeIndex];
+    console.log(challengeArray);
+}
+
+
+}
+
+// Function used to generated a random number./
+function getRandomInt() {
+    let max = sourceMovies.length
+    let challengeItem = Math.floor(Math.random() * max); 
+    return challengeItem
 }
 
 function letterSelection(){
@@ -14,8 +29,8 @@ function playAgain(){
     
 }
 
-let sourceMovies = ["Terminator", "The Dark Knight"]
-let sourceComics = ["Batman", "Superman"]
+let sourceMovies = ["Terminator", "The Dark Knight", "Aliens", "The Big Short"]
+let sourceComics = ["Batman", "Superman", "Lucifer", "Wolverine", "Daredevil"]
 
 document.addEventListener("DOMContentLoaded", function() {
     
@@ -24,9 +39,9 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
            let sourceType = this.innerHTML;
-            alert(`You clicked ${sourceType}`);
-            // Pending to add a redirect to the game.html.
+           newGame(sourceType);
         });
-    }
+        
+    }  
 });
 
