5baadd268dbfa0e238690ff9b71fb8a120c97221
Raul <raul@Rauls-MacBook-Pro.local>
2022-04-05 16:57:35 +0100

    Refactor to simplify function invoking

--- script.js
+++ script.js
@@ -2,15 +2,26 @@
  * This is invoked after the page loads and the user selects the title source for the game. 
  */
 function newGame(sourceType){
-    
+
+// Arrays to store the name of the callenges.
+let sourceMovies = ["Terminator", "The Dark Knight", "Aliens", "The Big Short"]
+let sourceComics = ["Batman", "Superman", "Lucifer", "Wolverine", "Daredevil"]
+
+let phrase = document.getElementById("gameCategoryPhrase");
+
+
+let challengeTitleLetter = [];
+let pickupLetters = []
+   
+
     if (sourceType === "Movies") {
     let challengeRandomIndex = getRandomInt();
     let challengeTitle = sourceMovies[challengeRandomIndex];
     generateChallenge(challengeTitle);
-    interactiveKeyboard();
+    interactiveKeyboard(challengeTitle);
     }
-}
 
+    
 // Function used to generated a random number
 function getRandomInt() {
     let max = sourceMovies.length
@@ -27,47 +38,39 @@ function generateChallenge(challengeTitle){
             challengeTitleLetter.push("<span>_</span>")
         }
       } 
-    document.getElementById("challengeGuessLetters").innerHTML =  challengeTitleLetter.join(" ");  
-}
-
-function hintDisplay(){
-}
-
-function playAgain(){  
+   document.getElementById("challengeGuessLetters").innerHTML =  challengeTitleLetter.join(" "); 
+    console.log(challengeTitleLetter); 
 }
 
 // Function to make the keyboard clickable and change style when a key is pressed.
-function interactiveKeyboard(){
+function interactiveKeyboard(challengeTitle){
     let keys = document.getElementsByClassName("key");
 
  for (let key of keys) {
         key.addEventListener("click", function() {
            let keyClicked = this.innerHTML;
             key.style.backgroundColor = "blue";
-           console.log(keyClicked);
-           checkLetter(keyClicked, challengeTitle)
+            pickupLetters.push(keyClicked);
+            console.log(pickupLetters);
+            for (let l of pickupLetters){
+                for ( let c of challengeTitle){
+                    if (l === c){
+                            document.getElementById("challengeGuessLetters").innerHTML =  keyClicked;
+                            console.log("match")
+                    }else{
+                        console.log("not match lose 1 life")
+                    }
+              } 
+            } 
         });          
     }  
 }
-
-// Iterated through the challenge to find a match
-function checkLetter(keyClicked,challengeTitle){
-    for (let c of challengeTitle) {
-        if (keyClicked == c){
-          console.log("match")
-        }
-        };
 }
 
-// Arrays to store the name of the callenges.
-let sourceMovies = ["Terminator", "The Dark Knight", "Aliens", "The Big Short"]
-let sourceComics = ["Batman", "Superman", "Lucifer", "Wolverine", "Daredevil"]
 
-let templatePhrase = "<h1> You have selected the Movies category </h1>";
-let phrase = document.getElementById("gameCategoryPhrase");
-let challengeTitleLetter = [];
 
-// The below create event listener for clicks.
+
+// The below create event listener for clicks after the page loads
 document.addEventListener("DOMContentLoaded", function() {
     
     let buttons = document.getElementsByTagName("button");
@@ -81,4 +84,3 @@ document.addEventListener("DOMContentLoaded", function() {
     newGame();
     
 });
-
