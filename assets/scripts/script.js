ca849bbec8d9b386589f93770212ecef6b36bfc1
Raul <raul@Rauls-MacBook-Pro.local>
2022-05-04 11:20:12 +0100

    Fix bug in issue #25 pointer event disable
    Fix bug that allowed user to click again in currently
    selected key. This could have caused a poor user experience.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -97,6 +97,7 @@ function interactiveKeyboard(challengeTitle) {
     for (let key of keys) {
         key.addEventListener("click", function () {
             let keyClicked = this.innerHTML;
+            key.style.pointerEvents = "none";
             key.style.backgroundColor = "#6868e9";
             key.style.color = "#d9cbcb";
             checkChallenge(keyClicked, challengeTitle);
